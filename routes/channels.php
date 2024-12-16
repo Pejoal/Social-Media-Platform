71768678a05eb3e517b5efed5b3fe1b8bf7568e1
pejoal <pejoal.official@gmail.com>
2024-12-16 20:15:31 +0100

    Add broadcast channel for public chat and improve authorization logic for group chats

--- routes/channels.php
+++ routes/channels.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\ChatGroup;
 use App\Models\User;
 use App\Services\FriendshipService;
 use Illuminate\Support\Facades\Broadcast;
@@ -20,6 +19,10 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
   return (int) $user->id === (int) $id;
 });
 
+Broadcast::channel('chat', function () {
+  return true;
+});
+
 Broadcast::channel('chat.{roomId}', function (User $user, int $roomId) {
   return ['id' => $user->id, 'firstname' => $user->firstname, 'lastname' => $user->lastname];
 });
@@ -32,7 +35,7 @@ Broadcast::channel('friend.chat.{friendshipId}', function (User $user, $friendsh
 });
 
 Broadcast::channel('chat.group.{chatGroupId}', function (User $user, int $chatGroupId) {
-  if(!$user->joinedChatGroups->contains($chatGroupId)) {
+  if (!$user->joinedChatGroups->contains($chatGroupId)) {
     abort(403, 'Not Authorized');
   }
   return ['id' => $user->id];
